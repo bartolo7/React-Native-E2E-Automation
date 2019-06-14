9fec572c19193bd28add44f91ea1444d1b9eb197
David Bartolomé <David.Bartolome@nordnet.se>
2019-06-14 13:00:18 +0200

    changes

--- automationFramework/e2e/Example.test.js
+++ automationFramework/e2e/Example.test.js
@@ -1,15 +1,8 @@
 import { driver, isAndroid } from "../../configurations/driverConfig";
 import {
-  menu,
-  instrument,
-  searchAction,
-  loginAction,
-  profile,
-  profileAction,
-  accountOverview,
-  order
+  
 } from "../screens";
-import { user, asserter, constant } from "../../utils";
+import { user, constant } from "../../utils";
 
 const { user1, user2 } = user;
 const { LANGUAGE } = constant;
@@ -20,19 +13,13 @@ describe("Login username and password", () => {
   });
   
   it(`Login example 1`, async () => {
-    await menu.tabAccount().click();
-    await loginAction.loginWithUsernamePassword(swedishUser);
-    const view = await accountOverview.screen();
-    view.isDisplayed();
+   ;
+    //view.isDisplayed();
   });
 
   it(`Login example 2`, async () => {
-    await menu.tabOrder().click();
-    await loginAction.loginWithUsernamePassword(swedishUser);
-    /* Jira ticket for IOS MD-4156 that´s why the if condition*/
-    if (isAndroid) {
-      const view = await order.screen();
-      view.isDisplayed();
+    
+      //view.isDisplayed();
     }
   });
 
